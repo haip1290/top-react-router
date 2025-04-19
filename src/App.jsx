581b5b3c8a2524702710b56a99c71521dc88f501
Hai Pham <thanh_hai1290@yahoo.com>
2025-04-19 20:43:37 +0700

    feat: add basic router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello from the main page</h1>
+      <p>Here are some examples of links to other pages </p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default App;
